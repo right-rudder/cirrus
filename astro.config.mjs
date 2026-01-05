0417ed9ab02f34151898fa3a458fa4f4cdb43cfc
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-01-05 19:37:13 -0300

    Install packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://cirrusaviation.com',
+	integrations: [mdx(), sitemap(),react()],
+	vite: {
+		plugins: [tailwindcss()],
+	}
 });
