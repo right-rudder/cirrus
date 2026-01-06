777c66e687c1b48dcd9c7850b659e5cb48e17742
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-01-06 19:41:08 -0300

    Create card components

---
+++ src/components/cards/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = ({ Icon, card }) => {
+  return (
+    <div class="p-4 flex flex-col gap-4 items-center text-center justify-between">
+      {Icon && <Icon className="text-4xl text-rose-500" />}
+      <p class="text-slate-600 font-semibold text-lg">{card.title}</p>
+      <p class="text-slate-500 text-base">{card.description}</p>
+      <a class="btn-secondary text-base" href={card.cta.url} target={card.cta.url.includes("http") ? "_blank" : ""}>{card.cta.text}</a>
+    </div>
+  );
+};
+
+export default Card;
