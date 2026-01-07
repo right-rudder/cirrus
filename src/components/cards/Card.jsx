8d339bf8460a8edc00c27bda1e6cc25c48c3440e
Guilherme Nunes <guilherme.j.r.nunes@gmail.com>
2026-01-07 20:47:28 -0300

    Small improvements

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Card = ({ Icon, card }) => {
   return (
     <div class="p-4 flex flex-col gap-4 items-center text-center justify-between">
-      {Icon && <Icon className="text-4xl text-rose-500" />}
-      <p class="text-slate-600 font-semibold text-lg">{card.title}</p>
-      <p class="text-slate-500 text-base">{card.description}</p>
-      <a class="btn-secondary text-base" href={card.cta.url} target={card.cta.url.includes("http") ? "_blank" : ""}>{card.cta.text}</a>
+      {Icon && <Icon className="text-5xl text-rose-500" />}
+      <p class="text-slate-600 font-semibold text-xl">{card.title}</p>
+      <p class="text-slate-500 text-lg">{card.description}</p>
+      <a class="btn-secondary text-lg" href={card.cta.url} target={card.cta.url.includes("http") ? "_blank" : ""}>{card.cta.text}</a>
     </div>
   );
 };
