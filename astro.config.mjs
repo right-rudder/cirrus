fcf63cd4b2cc811645502ad31cd1e4529732ea5a
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-01-09 12:19:25 -0300

    Add env configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,26 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://cirrusaviation.com',
-	integrations: [mdx(), sitemap(),react()],
-	vite: {
-		plugins: [tailwindcss()],
-	}
+  site: "https://cirrusaviation.com",
+  integrations: [mdx(), sitemap(), react()],
+  env: {
+    schema: {
+      PORTAL_URL: envField.string({ context: "client", access: "public", optional: false }),
+      PORTAL_API_KEY: envField.string({ context: "client", access: "public", optional: false }),
+      SCHOOL_RANDOM_ID: envField.string({ context: "client", access: "public", optional: false }),
+      /* ENROLL_WEBHOOK_URL: envField.string({ context: "client", access: "public", optional: false }),
+      CONTACT_US_WEBHOOK_URL: envField.string({ context: "client", access: "public", optional: false }),
+      DISCOVERY_SESSION_WEBHOOK_URL: envField.string({ context: "client", access: "public", optional: false }), */
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
