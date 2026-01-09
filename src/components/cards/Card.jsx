012a4e192583487a48df031eb3aa0096188294aa
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-01-08 21:09:09 -0300

    Minor fix

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Card = ({ Icon, card }) => {
   return (
-    <div class="p-4 flex flex-col gap-4 items-center text-center justify-between">
+    <div className="p-4 flex flex-col gap-4 items-center text-center justify-between">
       {Icon && <Icon className="text-5xl text-rose-500" />}
-      <p class="text-slate-600 font-semibold text-xl">{card.title}</p>
-      <p class="text-slate-500 text-lg">{card.description}</p>
-      <a class="btn-rose text-lg" href={card.cta.url} target={card.cta.url.includes("http") ? "_blank" : ""}>{card.cta.text}</a>
+      <p className="text-slate-600 font-semibold text-xl">{card.title}</p>
+      <p className="text-slate-500 text-lg">{card.description}</p>
+      <a className="btn-rose text-lg" href={card.cta.url} target={card.cta.url.includes("http") ? "_blank" : ""}>{card.cta.text}</a>
     </div>
   );
 };
