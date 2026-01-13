13f663472ec0a856e86fd8204180657458df225c
GuiJRNunes <116040992+GuiJRNunes@users.noreply.github.com>
2026-01-13 19:04:16 -0300

    Add animations

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Card = ({ Icon, card }) => {
+const Card = ({ Icon, card, index }) => {
   return (
-    <div className="p-4 flex flex-col gap-4 items-center text-center justify-between">
+    <div data-aos="fade-right" data-aos-delay={(index * 50) + 200} className="p-4 flex flex-col gap-4 items-center text-center justify-between">
       {Icon && <Icon className="text-5xl text-rose-500" />}
       <p className="text-slate-600 font-semibold text-xl">{card.title}</p>
       <p className="text-slate-500 text-lg">{card.description}</p>
